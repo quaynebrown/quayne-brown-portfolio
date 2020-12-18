bda3d8263a37e6acda4b2de7a2dcf94019e7a043
quaynebrown <quayne.brown@gmail.com>
2020-12-17 19:54:49 -0500

    mobile dropdown menu implemented

--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,8 @@
-const app = {};
+const app = {
+    menuBtn: '',
+    menuOpen: false
+};
+
 
 app.handleHoverIn =  function() {
     $(this).find('div').addClass('toggle-btn');
@@ -14,9 +18,23 @@ app.eventHandlers = () => {
 
     $project.hover(app.handleHoverIn, app.handleHoverOut);
 
+    // menu button
+    app.menuBtn.on('click', () => {
+        if(!app.menuOpen){
+            app.menuBtn.addClass('open');
+            app.nav.addClass('show-dropdown');
+            app.menuOpen = true
+        }else{
+            app.menuBtn.removeClass('open');
+            app.nav.removeClass('show-dropdown');
+            app.menuOpen = false
+        }
+    })
 }
 
 app.init = () => {
+    app.menuBtn = $('.menu-btn');
+    app.nav = $('header ul');
     app.eventHandlers();
 } 
 
