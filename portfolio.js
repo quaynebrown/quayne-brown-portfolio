8c4362a099d85b40cade422473f6c545b01d06e4
quaynebrown <quayne.brown@gmail.com>
2020-12-17 21:19:30 -0500

    Fixed header background on mobile

--- portfolio.js
+++ portfolio.js
@@ -23,10 +23,12 @@ app.eventHandlers = () => {
         if(!app.menuOpen){
             app.menuBtn.addClass('open');
             app.nav.addClass('show-dropdown');
+            app.listItems.addClass('show-list-items');
             app.menuOpen = true
         }else{
             app.menuBtn.removeClass('open');
             app.nav.removeClass('show-dropdown');
+            app.listItems.removeClass('show-list-items');
             app.menuOpen = false
         }
     })
@@ -35,6 +37,7 @@ app.eventHandlers = () => {
 app.init = () => {
     app.menuBtn = $('.menu-btn');
     app.nav = $('header ul');
+    app.listItems = $('header li');
     app.eventHandlers();
 } 
 
